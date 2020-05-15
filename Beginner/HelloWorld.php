2f6a9e9564856a5b6d13f69d79b67c66ca2330c1
milone365 <49542221+milone365@users.noreply.github.com>
2020-05-15 20:45:54 +0900

    Add files via upload

---
+++ Beginner/HelloWorld.php
@@ -0,0 +1,20 @@
+<!implentate php to html> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf8_encode">
+<!create background color> 
+<style>
+div{background-color:#ccc;width:150px;}
+</style>
+<title>Tel chi el telun </title>
+</head>
+<body>
+<div>
+<?php
+/*print string*/ 
+echo "HelloWORLD";
+?>
+</div>
+</body>
+</html>
