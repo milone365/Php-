312a50b82ecc6a86c327afcb874d55038491b9a2
milone365 <49542221+milone365@users.noreply.github.com>
2020-05-15 21:00:22 +0900

    Add files via upload

---
+++ Beginner/Variables text.php
@@ -0,0 +1,12 @@
+<?php
+//create string variable called name 
+$name="Robo";
+?>
+<html>
+<head>
+<title>impelenting php</title>
+</head>
+<body>
+<h1><?php echo "Hello ".$name?></h1>
+</body>
+</html>
